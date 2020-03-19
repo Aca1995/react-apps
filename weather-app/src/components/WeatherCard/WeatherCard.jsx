e5a45329d318f9a4344180da2b790c0f78b1ce11
Aleksandar Petrovic <al.petrovic@levi9.com>
2020-03-19 18:20:42 +0100

    create search box and weather info

---
+++ weather-app/src/components/WeatherCard/WeatherCard.jsx
@@ -0,0 +1,12 @@
+// import React from "react";
+
+// import Location from "../Location/Location";
+// import "./WeatherCard.css";
+
+// const WeatherCard = ({ temp }) => {
+//   return (
+
+//   );
+// };
+
+// export default WeatherCard;
